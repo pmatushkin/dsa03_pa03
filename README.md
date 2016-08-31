ff592b906e03499f80cb565e526f828d85dfdbdd
pmatushkin <pmatushkin@gmail.com>
2016-08-31 19:57:47 -0400

    feat: Pass Problem 2: Checking whether a Graph is Bipartite

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ### Problems
 Practice implementing algorithms for finding shortest paths in graphs:
 * [`/src/bfs/BFS.java`](src/bfs/BFS.java): Problem 1, Computing the Minimum Number of Flight Segments, computes the minimum number of flight segments to get from one city to another one (PASSED);
-* [`/src/bipartite/Bipartite.java`](src/bipartite/Bipartite.java): Problem 2, Checking whether a Graph is Bipartite, checks whether a graph is bipartite.
+* [`/src/bipartite/Bipartite.java`](src/bipartite/Bipartite.java): Problem 2, Checking whether a Graph is Bipartite, checks whether a graph is bipartite (PASSED).
--- src/bipartite/Bipartite.java
+++ src/bipartite/Bipartite.java
@@ -6,7 +6,57 @@ import java.util.Scanner;
 public class Bipartite {
     private static int bipartite(ArrayList<Integer>[] adj) {
         //write your code here
-        return -1;
+
+        // initialize the top node
+        Integer first = 0;
+
+        // initialize the array of colors
+        int[] colors = new int[adj.length];
+        colors[first] = 1;
+
+        // initialize the queue
+        Queue<Integer> q = new LinkedList<>();
+        q.add(first);
+
+        boolean isBipartite = true;
+
+        // begin traversing a graph
+        while (!q.isEmpty()) {
+            // get the first node in the queue
+            first = q.remove();
+
+            // get the nodes adjacent to the first node in the queue
+            ArrayList<Integer> first_adj = adj[first];
+
+            // get the color of the first node in the queue,..
+            int color_first = colors[first];
+            // change it to obtain the color for the adjacent nodes
+            int color_adj = color_first * -1;
+
+            // for every node adjacent to first...
+            for (Integer v : first_adj) {
+                // if it wasn't visited yet...
+                if (colors[v] == 0) {
+                    // queue it...
+                    q.add(v);
+                    // ... and color it
+                    colors[v] = color_adj;
+                } else if (colors[v] == color_first) {
+                    // we can break early if the adjacent node
+                    // was visited, and is marked the same color
+                    // as the first node in the queue
+                    isBipartite = false;
+                    break;
+                }
+            }
+
+            // breaking early
+            if (!isBipartite) {
+                break;
+            }
+        }
+
+        return isBipartite ? 1 : 0;
     }
 
     public static void main(String[] args) {
